a9bc9fc70b3b41e0dde82abde302a42baeb0f418
Lain <ren_white@ezweb.ne.jp>
2019-08-28 13:44:00 +0900

    first commit

---
+++ src/main/java/com/example/sampleproject/trySpring/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.sampleproject.trySpring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String Hello() {
+        return "hello";
+    }
+}
