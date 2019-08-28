f886f20f116982eb07c17583a4a3c792403f205d
Lain <ren_white@ezweb.ne.jp>
2019-08-29 00:46:34 +0900

    add Form

--- src/main/java/com/example/sampleproject/trySpring/HelloController.java
+++ src/main/java/com/example/sampleproject/trySpring/HelloController.java
@@ -1,12 +1,22 @@
-package com.example.sampleproject.trySpring;
+package com.example.sampleproject.tryspring;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
     @GetMapping("/hello")
     public String Hello() {
         return "hello";
+    }
+
+    @PostMapping("hello")
+    public String PostRequest(@RequestParam("text1")String str, Model model) {
+        // attributeにkeyとvalueを登録
+        model.addAttribute("sample", str);
+        return "helloResponse";
     }
 }
