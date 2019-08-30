88f3158fe609c37e46c5c81897e8bc6da729e861
Lain <ren_white@ezweb.ne.jp>
2019-08-31 02:26:18 +0900

    add AOP for Log

---
+++ src/main/java/com/example/sampleproject/login/aspect/LogAspect.java
@@ -0,0 +1,38 @@
+package com.example.sampleproject.login.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * LogAspect
+ */
+@Aspect
+@Component
+public class LogAspect {
+
+    /**
+     * スタートログ
+     * 
+     * @param jp
+     */
+    @Around("execution(* *..*.*Controller.*(..))")
+    public Object startLog(ProceedingJoinPoint jp) throws Throwable {
+        System.out.println("メソッド開始：" + jp.getSignature());
+
+        try {
+            Object result = jp.proceed();
+
+            System.out.println("メソッド終了：" + jp.getSignature());
+
+            return result;
+
+        } catch (Exception e) {
+            System.out.println("メソッド以上終了：" + jp.getSignature());
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+}
