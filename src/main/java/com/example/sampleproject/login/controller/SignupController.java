1e35250bcfb82e1534468fbbe2e89f0df98e11b6
Lain <ren_white@ezweb.ne.jp>
2019-08-30 21:32:15 +0900

    エラーメッセージの追加

--- src/main/java/com/example/sampleproject/login/controller/SignupController.java
+++ src/main/java/com/example/sampleproject/login/controller/SignupController.java
@@ -8,6 +8,7 @@ import com.example.sampleproject.login.domain.model.SignupForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,7 +66,7 @@ public class SignupController {
      * @return login.htmlに遷移
      */
     @PostMapping("/signup")
-    public String postSignU(@ModelAttribute SignupForm form, BindingResult bindingResult, Model model) {
+    public String postSignU(@ModelAttribute @Validated SignupForm form, BindingResult bindingResult, Model model) {
         // 入力チェックに引っかかった場合、ユーザー登録画面に戻る
         if (bindingResult.hasErrors()) {
             return getSignUp(form, model);
--- src/main/java/com/example/sampleproject/login/domain/model/SignupForm.java
+++ src/main/java/com/example/sampleproject/login/domain/model/SignupForm.java
@@ -2,6 +2,15 @@ package com.example.sampleproject.login.domain.model;
 
 import java.util.Date;
 
+import javax.validation.constraints.AssertFalse;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
@@ -13,34 +22,44 @@ import lombok.Data;
 public class SignupForm {
 
     /**
-     * ユーザーID
+     * ユーザーID 入力必須、メールアドレス形式
      */
+    @NotBlank // 入力必須
+    @Email // メールアドレス形式
     private String userId;
 
     /**
-     * パスワード
+     * パスワード 入力必須、長さ4から100まで、半角英数字のみ
      */
+    @NotBlank
+    @Length(min = 4, max = 100)
+    @Pattern(regexp = "^[a-zA-Z0-9]+$")
     private String password;
 
     /**
-     * ユーザー名
+     * ユーザー名 入力必須
      */
+    @NotBlank
     private String userName;
 
     /**
-     * 誕生日
+     * 誕生日 入力必須、yyyy/MM/dd形式
      */
+    @NotNull
     @DateTimeFormat(pattern = "yyyy/MM/dd")
     private Date birthday;
 
     /**
-     * 年齢
+     * 年齢 値が20から100まで
      */
+    @Min(20)
+    @Max(100)
     private int age;
 
     /**
-     * 結婚ステータス
+     * 結婚ステータス Falseのみ可能
      */
+    @AssertFalse
     private boolean marriage;
 
 }
